823f0933e8f52fb57d81ae89af859dc179c18c31
Faizullah-Laghari <faizullahls123@gmail.com>
2024-01-24 15:06:51 +0500

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,31 @@
+// import React from "react";
+// import SEO from "../common/seo";
+// import HomeOne from "../components/homes/home";
+// import Wrapper from "../layout/wrapper";
+
+// const Home = () => {
+//   return (
+//     <Wrapper>
+//       <SEO pageTitle={"Softec - Data analytics"} />
+//       <HomeOne />
+//     </Wrapper>
+//   );
+// };
+
+// export default Home;
+
+import React from "react";
+import SEO from "../common/seo";
+import HomeTwo from "../components/homes/home-2";
+import Wrapper from "../layout/wrapper";
+
+const index = () => {
+  return (
+    <Wrapper>
+      <SEO pageTitle={"Nextgen Payment"} />
+      <HomeTwo />
+    </Wrapper>
+  );
+};
+
+export default index;
